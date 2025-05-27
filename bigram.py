49ad6bdfed4c4527a21b917d96e07cc0ac2e059d
Holun <153267889+Hlnchng@users.noreply.github.com>
2025-05-27 13:26:45 +1200

    Updated the GPT architecture

--- bigram.py
+++ bigram.py
@@ -8,9 +8,13 @@ batch_size = 32 # how many independent sequences will we process in parallel?
 block_size = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 1e-3 # learning rate for the optimiser
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # runs on GPU instead
 eval_iters = 200
+n_embd = 32 # size of the embedding vector for each token
+n_head = 
+n_layer = 
+dropout = 
 # ------------
 
 script_dir = os.path.dirname(__file__)
@@ -60,18 +64,98 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    """one head of self-attention"""
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size))) # lower triangular matrix
+
+        self.dropout = nn.Dropout(dropout) # dropout layer for regularisation
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x) # (B,T,C)
+        q = self.query(x) # (B,T,C)
+        v = self.value(x) # (B,T,C)
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # (B,T,C) @ (B,C,T) -> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf')) # zero out the upper triangular part
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei) # apply dropout to the attention weights
+
+        out = wei @ v # (B,T,T) @ (B,T,C) -> (B,T,C)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """multiple heads of self-attention in parallel"""
+    
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1) # concatenate the outputs of all heads
+        out = self.proj(out)
+        return out  # (B,T,C) output of the multi-head attention layer
+
+class FeedForward(nn.Module):
+    """a simple linear layer followed by a non-linearity"""
+    
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd), # first linear layer
+            nn.ReLU(), # activation function
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+        
+    def forward(self, x):
+        return self.net(x) # apply the feedforward network
+
+class Block(nn.Module):
+    """Transformer block: communication followed by computation"""
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: number of attention heads
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size) # self-attention layer
+        self.ffwd = FeedForward(n_embd) # feedforward network
+        self.ln1 = nn.LayerNorm(n_embd) # layer normalisation after self-attention
+        self.ln2 = nn.LayerNorm(n_embd) # layer normalisation after feedforward network
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x)) # uses residual connections (layer normalisation is applied before self-attention)
+        x = x + self.ffwd(self.ln2(x)) # uses residual connections (layer normalisation is applied before feedforward network)
+        return x
+
 # Bigram model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd) 
+        self.position_embedding_table = nn.Embedding(block_size, n_embd) # block size will get it's own embedding vector
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)]) 
+        self.ln_f = nn.LayerNorm(n_embd) # final layer normalisation
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
+        B, T = idx.shape
 
         # idx and targets are both (B,T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        x = tok_emb + pos_emb # (B,T,C) elementwise addition of token and position embeddings
+        x = self.block(x) # (B,T,C) pass through the transformer blocks
+        logits = self.lm_head(x) # (B,T,vocab_size)
 
         if targets is None:
             loss = None
@@ -86,8 +170,9 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
-            # get the predictions
-            logits, loss = self(idx)
+            idx_cond = idx[:, -block_size:] # (B, T) get the last block_size tokens
+            # get the predictions and loss
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B, C)
             # apply softmax to get probabilities
@@ -98,7 +183,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device) # model is moved to the device (GPU)
 
 # create a PyTorch optimiser
