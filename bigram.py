b6f49ef701bf8dfba219f85d9ddec1679d5aee81
Holun <153267889+Hlnchng@users.noreply.github.com>
2025-05-27 18:53:21 +1200

    Updated GPT architecture
    - Included Wilfred Owen poetry dataset

--- bigram.py
+++ bigram.py
@@ -4,17 +4,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 64 # how many independent sequences will we process in parallel?
-block_size = 256 # what is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4 # learning rate for the optimiser
-device = 'cuda' if torch.cuda.is_available() else 'cpu' # runs on GPU instead
-eval_iters = 200
-n_embd = 384 # size of the embedding vector for each token
-n_head = 6
-n_layer = 6
-dropout = 0.2 # dropout rate for regularisation
+batch_size = 16      
+block_size = 64      
+max_iters = 5000      
+eval_interval = 50   
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 40
+n_embd = 128         
+n_head = 4           
+n_layer = 4         
+dropout = 0.15       
 # ------------
 
 script_dir = os.path.dirname(__file__)
